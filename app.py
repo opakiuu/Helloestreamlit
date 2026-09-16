3e1d2f6bc87a821c1891dd058b2c26f91308e803
opakiuu <opakiuu@gmail.com>
2026-09-16 21:55:35 +1000

    7

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if st.button("Generate"):
     art = cv2.imread("art.jpg")
     background = cv2.imread("background.jpg")
 
-    art = cv2.resize(art, (400, 400))
+    art = cv2.resize(art, (size, size))
 
     mask = np.any(art < 250, axis=2)
 
